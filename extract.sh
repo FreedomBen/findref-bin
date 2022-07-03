c4079231514ef782f973d922bcb48e021920fe9c
Benjamin Porter <FreedomBen@users.noreply.github.com>
2022-07-03 16:53:35 -0600

    update extract.sh for BSD

--- extract.sh
+++ extract.sh
@@ -3,6 +3,11 @@
 # shellcheck disable=SC2164
 
 # Hacky script to rename for easier uploading github assets
+#
+# Useful for creating a Github "release" (rather than just tagging)
+#
+# Run this from the current directory.  It will create/populate the
+# 'uploads' dir that you can then upload to github through the release UI
 
 if [ -z "$1" ]; then
   echo "Pass first arg as version number"
@@ -10,13 +15,15 @@ if [ -z "$1" ]; then
 fi
 
 mkdir -p upload
-cd "$1/darwin/386"
+cd "$1/darwin/arm64"
 
-cp findref.zip "../../../upload/findref-$1-darwin-386.zip"
+# Darwin
+cp findref.zip "../../../upload/findref-$1-darwin-arm64.zip"
 cd "../amd64"
 cp findref.zip "../../../upload/findref-$1-darwin-amd64.zip"
-cd "../../linux/386"
 
+# Linux
+cd "../../linux/386"
 cp findref.zip "../../../upload/findref-$1-linux-386.zip"
 cd "../amd64"
 cp findref.zip "../../../upload/findref-$1-linux-amd64.zip"
@@ -24,8 +31,21 @@ cd "../arm"
 cp findref.zip "../../../upload/findref-$1-linux-arm.zip"
 cd "../arm64"
 cp findref.zip "../../../upload/findref-$1-linux-arm64.zip"
-cd "../../windows/386"
 
+# Windows
+cd "../../windows/386"
 cp findref.zip "../../../upload/findref-$1-windows-386.zip"
 cd "../amd64"
 cp findref.zip "../../../upload/findref-$1-windows-amd64.zip"
+
+# FreeBSD
+cd "../../freebsd/arm64"
+cp findref.zip "../../../upload/findref-$1-freebsd-arm64.zip"
+cd "../amd64"
+cp findref.zip "../../../upload/findref-$1-freebsd-amd64.zip"
+
+# OpenBSD
+cd "../../openbsd/arm64"
+cp findref.zip "../../../upload/findref-$1-openbsd-arm64.zip"
+cd "../amd64"
+cp findref.zip "../../../upload/findref-$1-openbsd-amd64.zip"
